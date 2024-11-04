e53277709cc3183ea06809af887dd8a65287bd5d
Adhish Ramkumar <adhishramkumar@yahoo.com>
2024-11-03 19:34:05 -0500

    initial chess gui

--- bestchess.py
+++ bestchess.py
@@ -1,49 +1,283 @@
+# importing required librarys
+import pygame
 import chess
-import PySimpleGUI as sg
-import chess.svg
+import math
 
-def draw_board(board):
-    return chess.svg.board(board=board, size=400)
 
-def create_window(board):
-    layout = [
-        [sg.Image(data=draw_board(board), key='-BOARD-')],
-        [sg.Text("Enter your move (e.g. e2e4): "), sg.InputText(key='-MOVE-')],
-        [sg.Button('Make Move'), sg.Button('Reset')]
-    ]
-    return sg.Window('Chess Game', layout, finalize=True)
+#initialise display
+X = 800
+Y = 800
+scrn = pygame.display.set_mode((X, Y))
+pygame.init()
 
-def main():
-    board = chess.Board()
-    window = create_window(board)
+#basic colours
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+YELLOW = (204, 204, 0)
+BLUE = (50, 255, 255)
+BLACK = (0, 255, 0)
+TAN = (236, 218, 185)
+BROWN = (174, 138, 104)
 
-    while True:
-        try:
-            event, values = window.read()
+#initialise chess board
+b = chess.Board()
 
-            if event == sg.WIN_CLOSED:
-                break
+#load piece images
+pieces = {
+    'p': pygame.image.load('images/black_pawn.png').convert_alpha(),
+    'n': pygame.image.load('images/black_knight.png').convert_alpha(),
+    'b': pygame.image.load('images/black_bishop.png').convert_alpha(),
+    'r': pygame.image.load('images/black_rook.png').convert_alpha(),
+    'q': pygame.image.load('images/black_queen.png').convert_alpha(),
+    'k': pygame.image.load('images/black_king.png').convert_alpha(),
+    'P': pygame.image.load('images/white_pawn.png').convert_alpha(),
+    'N': pygame.image.load('images/white_knight.png').convert_alpha(),
+    'B': pygame.image.load('images/white_bishop.png').convert_alpha(),
+    'R': pygame.image.load('images/white_rook.png').convert_alpha(),
+    'Q': pygame.image.load('images/white_queen.png').convert_alpha(),
+    'K': pygame.image.load('images/white_king.png').convert_alpha(),
+}
 
-            if event == 'Make Move':
-                move = values['-MOVE-']
-                try:
-                    move = chess.Move.from_uci(move)
-                    if move in board.legal_moves:
-                        board.push(move)
-                        window['-BOARD-'].update(data=draw_board(board))
-                        window['-MOVE-'].update('')  # Clear input
+for key in pieces:
+    pieces[key] = pygame.transform.scale(pieces[key], (100, 100))
+
+def update(scrn,board):
+    '''
+    updates the screen basis the board class
+    '''
+    
+    for i in range(64):
+        piece = board.piece_at(i)
+        if piece == None:
+            pass
+        else:
+            scrn.blit(pieces[str(piece)],((i%8)*100,700-(i//8)*100))
+
+    pygame.display.flip()
+
+def drawBoard():
+    for i in range(8):
+        for j in range(8):
+            color = TAN if (i%2 == j%2) else BROWN
+            scrn.fill(color, pygame.Rect(j*100, i*100, 100, 100))
+
+def main(BOARD):
+
+    '''
+    for human vs human game
+    '''
+    #name window
+    pygame.display.set_caption('Chess')
+
+    drawBoard()
+
+    #variable to be used later
+    index_moves = []
+
+    status = True
+    while (status):
+        #update screen
+        update(scrn,BOARD)
+
+        for event in pygame.event.get():
+     
+            # if event object type is QUIT
+            # then quitting the pygame
+            # and program both.
+            if event.type == pygame.QUIT:
+                status = False
+
+            # if mouse clicked
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #remove previous highlights
+                # scrn.fill(BROWN)
+                #get position of mouse
+                pos = pygame.mouse.get_pos()
+
+                #find which square was clicked and index of it
+                square = (math.floor(pos[0]/100),math.floor(pos[1]/100))
+                index = (7-square[1])*8+(square[0])
+                
+                # if we are moving a piece
+                if index in index_moves: 
+                    
+                    move = moves[index_moves.index(index)]
+                    
+                    BOARD.push(move)
+
+                    drawBoard()
+
+                    #reset index and moves
+                    index=None
+                    index_moves = []
+                    
+                    
+                # show possible moves
+                else:
+                    #check the square that is clicked
+                    piece = BOARD.piece_at(index)
+                    #if empty pass
+                    if piece == None:
+                        
+                        pass
                     else:
-                        sg.popup_error("Illegal move!")
-                except:
-                    sg.popup_error("Invalid move format!")
+                        
+                        #figure out what moves this piece can make
+                        all_moves = list(BOARD.legal_moves)
+                        moves = []
+                        for m in all_moves:
+                            if m.from_square == index:
+                                
+                                moves.append(m)
 
-            if event == 'Reset':
-                board.reset()
-                window['-BOARD-'].update(data=draw_board(board))
-        except any as e:
-            print(e)
+                                t = m.to_square
 
-    window.close()
+                                TX1 = 100*(t%8)
+                                TY1 = 100*(7-t//8)
 
-if __name__ == '__main__':
-    main()
+                                
+                                #highlight squares it can move to
+                                pygame.draw.rect(scrn,BLUE,pygame.Rect(TX1,TY1,100,100),5)
+                        
+                        index_moves = [a.to_square for a in moves]
+     
+    # deactivates the pygame library
+        if BOARD.outcome() != None:
+            print(BOARD.outcome())
+            status = False
+            print(BOARD)
+    pygame.quit()
+
+# def main_one_agent(BOARD,agent,agent_color):
+    
+#     '''
+#     for agent vs human game
+#     color is True = White agent
+#     color is False = Black agent
+#     '''
+    
+#     #make background black
+#     scrn.fill(BLACK)
+#     #name window
+#     pygame.display.set_caption('Chess')
+    
+#     #variable to be used later
+#     index_moves = []
+
+#     status = True
+#     while (status):
+#         #update screen
+#         update(scrn,BOARD)
+        
+     
+#         if BOARD.turn==agent_color:
+#             BOARD.push(agent(BOARD))
+#             scrn.fill(BLACK)
+
+#         else:
+
+#             for event in pygame.event.get():
+         
+#                 # if event object type is QUIT
+#                 # then quitting the pygame
+#                 # and program both.
+#                 if event.type == pygame.QUIT:
+#                     status = False
+
+#                 # if mouse clicked
+#                 if event.type == pygame.MOUSEBUTTONDOWN:
+#                     #reset previous screen from clicks
+#                     scrn.fill(BLACK)
+#                     #get position of mouse
+#                     pos = pygame.mouse.get_pos()
+
+#                     #find which square was clicked and index of it
+#                     square = (math.floor(pos[0]/100),math.floor(pos[1]/100))
+#                     index = (7-square[1])*8+(square[0])
+                    
+#                     # if we have already highlighted moves and are making a move
+#                     if index in index_moves: 
+                        
+#                         move = moves[index_moves.index(index)]
+#                         #print(BOARD)
+#                         #print(move)
+#                         BOARD.push(move)
+#                         index=None
+#                         index_moves = []
+                        
+#                     # show possible moves
+#                     else:
+                        
+#                         piece = BOARD.piece_at(index)
+                        
+#                         if piece == None:
+                            
+#                             pass
+#                         else:
+
+#                             all_moves = list(BOARD.legal_moves)
+#                             moves = []
+#                             for m in all_moves:
+#                                 if m.from_square == index:
+                                    
+#                                     moves.append(m)
+
+#                                     t = m.to_square
+
+#                                     TX1 = 100*(t%8)
+#                                     TY1 = 100*(7-t//8)
+
+                                    
+#                                     pygame.draw.rect(scrn,BLUE,pygame.Rect(TX1,TY1,100,100),5)
+#                             #print(moves)
+#                             index_moves = [a.to_square for a in moves]
+     
+#     # deactivates the pygame library
+#         if BOARD.outcome() != None:
+#             print(BOARD.outcome())
+#             status = False
+#             print(BOARD)
+#     pygame.quit()
+
+# def main_two_agent(BOARD,agent1,agent_color1,agent2):
+#     '''
+#     for agent vs agent game
+    
+#     '''
+  
+#     #make background black
+#     scrn.fill(BLACK)
+#     #name window
+#     pygame.display.set_caption('Chess')
+    
+#     #variable to be used later
+
+#     status = True
+#     while (status):
+#         #update screen
+#         update(scrn,BOARD)
+        
+#         if BOARD.turn==agent_color1:
+#             BOARD.push(agent1(BOARD))
+
+#         else:
+#             BOARD.push(agent2(BOARD))
+
+#         scrn.fill(BLACK)
+            
+#         for event in pygame.event.get():
+     
+#             # if event object type is QUIT
+#             # then quitting the pygame
+#             # and program both.
+#             if event.type == pygame.QUIT:
+#                 status = False
+     
+#     # deactivates the pygame library
+#         if BOARD.outcome() != None:
+#             print(BOARD.outcome())
+#             status = False
+#             print(BOARD)
+#     pygame.quit()
+
+main(b)
